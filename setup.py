f168fbff78340005c7061fd2d86ef11f4957111b
Martin <reg-github@z8i.de>
2021-01-06 20:14:21 +0100

    initial package format commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kloggpro", 
+    version="0.0.1",
+    author="Martin (z8i)",
+    author_email="github@z8i.de",
+    description="Package for TFA KlimaLoggPro-Driver to use with Home Assistant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/z8i/kloggpro",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
